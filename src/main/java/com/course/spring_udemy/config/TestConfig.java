f241f8905b011ec7d307e116705e2c064b81209d
Joao Nascimento <gustavojoao28@gmail.com>
2022-03-04 10:26:34 -0300

    Payment, one-to-one association

--- src/main/java/com/course/spring_udemy/config/TestConfig.java
+++ src/main/java/com/course/spring_udemy/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.course.spring_udemy.entities.Category;
 import com.course.spring_udemy.entities.Order;
 import com.course.spring_udemy.entities.OrderItem;
+import com.course.spring_udemy.entities.Payment;
 import com.course.spring_udemy.entities.Product;
 import com.course.spring_udemy.entities.User;
 import com.course.spring_udemy.entities.enums.OrderStatus;
@@ -81,5 +82,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
